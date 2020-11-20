a239e34d27ad82b92a21b9d784c4561fd0235212
aidansoffe <aidansoffe@gmail.com>
2020-11-20 15:25:24 -0800

    done 20

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -1,13 +1,8 @@
+import statistics
+
 def mode(nums):
-    """Return most-common number in list.
+    return (statistics.mode(nums))
 
-    For this function, there will always be a single-most-common value;
-    you do not need to worry about handling cases where more than one item
-    occurs the same number of times.
+        
 
-        >>> mode([1, 2, 1])
-        1
-
-        >>> mode([2, 2, 3, 3, 2])
-        2
-    """
+print(mode([2, 2, 3, 3, 3, 3, 3, 2]))
